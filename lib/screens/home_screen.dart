62321718cca83b52b30b68185f4f626cb7420f17
Diego Villafane <dievil0705@gmail.com>
2025-03-27 17:43:05 -0500

    feat: Agregado buscador, orden descendente de criptos y comentarios explicativos en el código

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,29 +1,25 @@
+// Importa el paquete material de Flutter, que provee componentes de UI.
 import 'package:flutter/material.dart';
+// Importa la pantalla que muestra los precios de criptomonedas.
 import 'crypto_prices_screen.dart';
 
+// Definición del widget HomeScreen que es de tipo StatelessWidget (no tiene estado mutable).
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  // Método build que construye la interfaz de usuario del widget.
   @override
   Widget build(BuildContext context) {
+    // DefaultTabController permite gestionar una vista con pestañas.
     return DefaultTabController(
-      length: 2,
+      length:
+          2, // Número de pestañas a controlar (aunque actualmente solo se utiliza una).
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('CoinCap API 2.0 Demo'),
-          //bottom: const TabBar(
-          //  indicatorColor: Colors.white,
-          //  tabs: [
-          //    Tab(text: 'Precios'),
-          //    Tab(text: 'Activos'),
-          //  ],
-          //),
-        ),
-        body: const TabBarView(
-          children: [
-            CryptoPricesScreen(),
-          ],
-        ),
+        // AppBar con título y opciones de navegación.
+        appBar: AppBar(title: const Text('CoinCap API 2.0 Demo')),
+        // Cuerpo de la pantalla que muestra las diferentes vistas asociadas a cada pestaña.
+        // Actualmente se muestra únicamente la pantalla de precios de criptomonedas.
+        body: const TabBarView(children: [CryptoPricesScreen()]),
       ),
     );
   }
